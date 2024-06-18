a5cc6e5455bb289015fffdf66e1a3981fe11cb26
Karl Johan Grahn <karl.johan@stakater.com>
2024-06-18 23:19:23 +0200

    update

--- v4/app.py
+++ v4/app.py
@@ -3,8 +3,8 @@ from opentelemetry.sdk.trace import TracerProvider
 
 from flask import Flask, request
 
-provider = TracerProvider()
-trace.set_tracer_provider(provider)
+# Now no traces are sent to console unless we specify that as exporter
+# with opentelemetry-instrument, because a consumer has not been configured
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
@@ -30,4 +30,4 @@ def sum_numbers(first: int, second: int) -> int:
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=3000)
